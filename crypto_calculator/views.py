96839140839df618b5c1a9b6526044445217aa6a
Bebesi László - PC <bebesilaci@gmail.com>
2024-03-24 19:53:52 +0100

    working submit button

--- crypto_calculator/views.py
+++ crypto_calculator/views.py
@@ -4,18 +4,22 @@ from crypto_calculator.models import RawPriceData
 from asgiref.sync import sync_to_async
 import pandas as pd
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 @csrf_exempt
-async def get_raw_price_data(request):
-    print(request.body.inputValue)
+def get_raw_price_data(request):
+    print(request.method)
     if request.method == "POST":
-        raw_price_data = await sync_to_async(list)(RawPriceData.objects.filter(symbol='BTC-USD').values())
-        df = await pd.DataFrame(list(raw_price_data))
-        json_data = await df.drop(columns=["id"]).to_dict()
-        return JsonResponse(json_data)
-    else:
-        return JsonResponse({})
+        all_input = json.loads(request.body.decode('utf-8'))
+        raw_price_data = RawPriceData.objects.filter(symbol=all_input["symbol"]).values("close", "date")
+        df = pd.DataFrame(list(raw_price_data))
+        df["date"] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+        if len(df) > 0:
+            json_data = {"raw_price": df.to_dict()}
+            return JsonResponse(json_data)
+    # default return
+    return {"ERROR_CODE": 404}
 
 
 async def get_available_symbols(request):
--- frontend/src/components/ExplorerPage.tsx
+++ frontend/src/components/ExplorerPage.tsx
@@ -1,59 +1,54 @@
-import {useEffect, SetStateAction, useState } from "react";
+import { SetStateAction, useState } from "react";
 import "./ExplorerPage.css";
 import { API } from "../Api";
 
 function ExplorerPage() {
+  const [jsonData, setJsonData] = useState(null);
+  const [inputValue, setInputValue] = useState("");
 
-    const [jsonData, setJsonData] = useState(null);
-    const [inputValue, setInputValue] = useState('');
+  const makeRequest = () => {
+    console.log("Input value:", inputValue);
+  };
 
-    const makeRequest = () => {
-        console.log('Input value:', inputValue);
-    };
+  const handleInputChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setInputValue(event.target.value);
+  };
 
-    const handleInputChange = (event: { target: { value: SetStateAction<string>; }; }) => {
-        setInputValue(event.target.value);
-    };
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-
-        try {
-        // Make a POST request to your backend
-        const response = await fetch(API + 'crypto/api/get_raw_price_data', {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            body: JSON.stringify({ inputString: inputValue }), // Send the input value
-        });
-        console.log(response);
-        if (response.ok) {
-            // Handle success (e.g., show a success message)
-            useEffect(() => {
-                // Simple data fetch
-                fetch(API + 'crypto/api/get_raw_price_data')  // fetch json value from url
-                    .then(response => response.json())
-                    .then(data => setJsonData(data))
-                    .catch(error => console.error('Error fetching data:', error));
-            }, []);
-            console.log('Data sent successfully!');
-        } else {
-            console.error('Error sending data to the backend.');
-        }
-        } catch (error) {
-        console.error('An error occurred:', error);
-        }
-    };
-
+    try {
+      const response = await fetch(API + "crypto/api/get_raw_price_data", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ symbol: inputValue }),
+      });
+      console.log(response);
+      if (response.ok) {
+        const data = await response.json();
+        setJsonData(data);
+        console.log("Data sent successfully!");
+      } else {
+        console.error("Error sending data to the backend.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
 
   return (
     <div className="parent-container">
       <link href="/ExplorerPage.css"></link>
-      <form  onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="ticker_input" className='ticker_input-label'>Choose a ticker</label>
+          <label htmlFor="ticker_input" className="ticker_input-label">
+            Enter a crypto symbol
+          </label>
           <input
             type="ticker_input"
             className="form-control"
@@ -62,13 +57,15 @@ function ExplorerPage() {
             value={inputValue}
             onChange={handleInputChange}
           ></input>
-          <button type="submit" className="btn btn-primary" onClick={makeRequest}>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={makeRequest}
+          >
             Explore
           </button>
         </div>
-        {jsonData && (
-                <pre>{JSON.stringify(jsonData, null, 2)}</pre>
-        )}
+        {jsonData && <pre>{JSON.stringify(jsonData, null, 2)}</pre>}
       </form>
     </div>
   );
