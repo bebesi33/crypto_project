4455e16aa14c243ac10b98b1043becb690b7590e
Bebesi László - PC <bebesilaci@gmail.com>
2024-04-07 17:59:11 +0200

    working data table react

--- frontend/src/components/tables/SimpleTable.tsx
+++ frontend/src/components/tables/SimpleTable.tsx
@@ -11,35 +11,25 @@ const SimpleTable: React.FC<TableProps> = ({
   metric_column,
   value_column,
 }) => {
-
-  const renderHeader = () => {
-    return (
-      <tr>
-        <th>{metric_column}</th>
-        <th>{value_column}</th>
-      </tr>
-    );
-  }; // end renderHeader
-
-  const renderRows = () => {
-    // Convert the Map to an array of entries
-    const dataEntries = Array.from(primary_data.entries());
-    console.log(dataEntries)
-    return (<div/>)
-    // return dataEntries.map(([name, value]) => (
-    //   <tr key={name}>
-    //     <td>{name}</td>
-    //     <td>{value}</td>
-    //   </tr>
-    // ));
-  }; // end renderRows
-  console.log("This is fine!")
+  console.log("This is fine!");
   return (
     <div style={{ margin: "50px" }}>
       <h1>{table_title}</h1>
       <table>
-        <thead>{renderHeader()}</thead>
-        <tbody>{renderRows()}</tbody>
+        <thead>
+          <tr>
+            <th>{metric_column}</th>
+            <th>{value_column}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(primary_data).map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   ); // end return
