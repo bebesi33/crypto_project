751c4c9a29be50070fcef696efd71422b4611ced
Bebesi László - PC <bebesilaci@gmail.com>
2024-09-08 16:36:06 +0200

    house keeping around web scraping

--- factor_model/model_update/loaders/crypto_coin_loaders.py
+++ factor_model/model_update/loaders/crypto_coin_loaders.py
@@ -1,10 +1,13 @@
-import requests
 import pandas as pd
 from requests_html import HTMLSession
-from typing import List
 
 
-def grab_yahoo_finance_symbols() -> List[str]:
+def grab_yahoo_finance_symbols() -> list[str]:
+    """Loads available crypto tickers from Yahoo Finance
+
+    Returns:
+        list[str]: list of tickers
+    """
     html_session = HTMLSession()
     currency_limit = 100  # this is the upper limit
 
@@ -19,22 +22,39 @@ def grab_yahoo_finance_symbols() -> List[str]:
 
 def grab_all_yahoo_finance_crypto_symbols(
     max_number_of_crypto: int = 3000,
-) -> List[str]:
+) -> list[str]:
+    """Loads available crypto tickers from Yahoo Finance
+
+    Args:
+        max_number_of_crypto (int, optional): maximum nubmer of coins retrieved. Defaults to 3000.
+
+    Returns:
+        list[str]: List of tickers
+    """
     html_session = HTMLSession()
-    currency_increment = 100  # this is the upper limit
+    currency_increment = 50  # this is the upper limit
     all_tickers = list()
     offset = 0
-    for i in range(0, int(max_number_of_crypto / 100) + 1):
+    for _ in range(0, int(max_number_of_crypto / currency_increment) + 1):
 
         resp = html_session.get(
-            f"https://finance.yahoo.com/crypto?count={currency_increment}&offset={offset}"
+            # f"https://finance.yahoo.com/crypto?count={currency_increment}&offset={offset}"
+            f"https://finance.yahoo.com/markets/crypto/all/?start={offset}&count={currency_increment}"
         )
         tables = pd.read_html(resp.html.raw_html)
         df = tables[0].copy()
         tickers = df.Symbol.tolist()
-        offset += 100
+        offset += currency_increment
         all_tickers = all_tickers + tickers
-    return all_tickers
+    # post processing if ticket contains spaces we use only the first half (true ticker)
+    # e.g.: 0X0-USD 0x0.ai USD translates to 0X0-USD
+    processed_tickers = []
+    for ticker in all_tickers:
+        if ticker.find(" ") > -1:
+            processed_tickers.append(ticker.split(" ")[0])
+        else:
+            processed_tickers.append(ticker)
+    return processed_tickers
 
 
 # to be kept for historical reason
@@ -92,11 +112,24 @@ TICKER_LIST = [
 ]
 
 
-def get_ticker_list() -> List[str]:
+def get_ticker_list() -> list[str]:
+    """Merges historic ticker list with latest ticker list from Yahoo Finance
+
+    Returns:
+        list[str]: ticker list
+    """
     return sorted(list(set(TICKER_LIST + grab_yahoo_finance_symbols())))
 
 
-def get_extended_ticker_list(max_number_of_crypto: int = 3000) -> List[str]:
+def get_extended_ticker_list(max_number_of_crypto: int = 3000) -> list[str]:
+    """"Merges historic ticker list with latest ticker list from Yahoo Finance
+
+    Args:
+        max_number_of_crypto (int, optional): _description_. Defaults to 3000.
+
+    Returns:
+        list[str]: list of tickers
+    """
     return sorted(
         list(
             set(
