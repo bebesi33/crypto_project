e35e138cb2f8e6c25de7dcaff09b417dc406b015
Bebesi László - PC <bebesilaci@gmail.com>
2024-09-22 16:07:04 +0200

    quick tutorial v1 finished

--- frontend/src/components/documentation/Tutorial.tsx
+++ frontend/src/components/documentation/Tutorial.tsx
@@ -1,51 +1,73 @@
-import React, { useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './Tutorial.css';
-import explorer_1 from "../assets/images/explorer1.jpg";
-import explorer_2 from "../assets/images/explorer2.jpg";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Carousel } from "react-bootstrap";
+import "./Tutorial.css";
+import explorer_1 from "../assets/images/explorer_1.jpg";
+import explorer_2 from "../assets/images/explorer_2.jpg";
+import risk_calc_1 from "../assets/images/risk_calc_1.jpg";
+import risk_calc_2 from "../assets/images/risk_calc_2.jpg";
 //  https://stackoverflow.com/questions/73017616/bootstrap-carousel-control-not-working-in-dom-manipulation
 
 const Tutorial: React.FC = () => {
-    useEffect(() => {
-        // Ensure Bootstrap's carousel is initialized
-        const carousel = document.querySelector('#tutorial');
-        if (carousel) {
-            // new window.bootstrap.Carousel(carousel);
-        }
-    }, []);
-
-    return (
-        <>
-        <div><h4>Quick Tutorial</h4></div>
-        <div id="tutorial" className="carousel slide" data-ride="carousel">
-        <ol className="carousel-indicators">
-            <li data-target="#tutorial" data-slide-to="0" className="active"></li>
-            <li data-target="#tutorial" data-slide-to="1"></li>
-            <li data-target="#tutorial" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-            <div className="carousel-item active">
-            <img className="d-block w-100" src={explorer_1} alt="First slide" />
-            </div>
-            <div className="carousel-item">
-            <img className="d-block w-100" src={explorer_2} alt="Second slide" />
-            </div>
-            <div className="carousel-item">
-            <img className="d-block w-100" src={explorer_1} alt="Third slide" />
-            </div>
-        </div>
-        <a className="carousel-control-prev" href="#tutorial" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#tutorial" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-        </a>
-        </div>
-        </>
-    );
-}
+  return (
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img className="d-block w-100" src={explorer_1} alt="Third slide" />
+        <Carousel.Caption>
+          <h3> Use the "Explorer" tool to check cryptocurrency coverage </h3>
+          <p>
+            <b>
+              The relevant user input covers the name of the cryptocurrency (1)
+              and half-life related parameters (2)
+            </b>
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={explorer_2} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>
+            {" "}
+            The "Explorer" tool results show the price history and risk estimates of the
+            selected cryptocurrency{" "}
+          </h3>
+          <p>
+            {" "}
+            <b>The debug messages may show problems with parameter settings </b>
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={risk_calc_1} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>
+            Use "Risk Calculator" tool to assess your portfolio's risk profile
+          </h3>
+          <p>
+            <b>
+              Provide the calculation date (1), provide portfolio compositons in
+              csv-s (2), specify risk calculation parameters (3)
+            </b>
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={risk_calc_2} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>
+            The "Risk Calculator" provides a detailed breakdown on the portfolio's risk profile
+          </h3>
+          <p>
+            <b>
+              The tool presents high level risk summary, and component level
+              breakdowns
+            </b>
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
 
 export default Tutorial;
