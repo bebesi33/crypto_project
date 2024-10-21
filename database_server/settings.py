7e2c5a9d8231ada7cfe5b72633d8a45953a90d8b
Bebesi László - PC <bebesilaci@gmail.com>
2024-10-21 20:07:18 +0200

    very crude prod deployment attempt with whitenoise

--- database_server/settings.py
+++ database_server/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iw0f7k^jcnf-ecrlp5qo=hf5_ub(^5su-q#(9orici+wg)iqjp'
+SECRET_KEY = "django-insecure-iw0f7k^jcnf-ecrlp5qo=hf5_ub(^5su-q#(9orici+wg)iqjp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 VITE_APP_DIR = os.path.join(BASE_DIR, "frontend")
 
@@ -36,77 +36,80 @@ DATABASE_LOCATION = os.path.join(BASE_DIR.parent, "database")
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'crypto_calculator'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "crypto_calculator",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-ROOT_URLCONF = 'database_server.urls'
+ROOT_URLCONF = "database_server.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/dist')
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "frontend/dist")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'database_server.wsgi.application'
+WSGI_APPLICATION = "database_server.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_LOCATION / Path('default.sqlite3'),
-        'TEST': {'DEPENDENCIES': [], 'NAME': 'test_default'},
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_LOCATION / Path("default.sqlite3"),
+        "TEST": {"DEPENDENCIES": [], "NAME": "test_default"},
     },
-    'raw_price_data':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_LOCATION / Path('raw_price_data.sqlite3'),
-        'TEST': {'DEPENDENCIES': [], 'NAME': 'test_raw_price_data'},
+    "raw_price_data": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_LOCATION / Path("raw_price_data.sqlite3"),
+        "TEST": {"DEPENDENCIES": [], "NAME": "test_raw_price_data"},
     },
-    'returns':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_LOCATION / Path('returns.sqlite3'),
-        'TEST': {'DEPENDENCIES': [],},
+    "returns": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_LOCATION / Path("returns.sqlite3"),
+        "TEST": {
+            "DEPENDENCIES": [],
+        },
     },
-    'factor_model_estimates':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_LOCATION / Path('factor_model_estimates.sqlite3'),
-        'TEST': {'DEPENDENCIES': [], 'NAME': 'test_factor_model_estimates'},
+    "factor_model_estimates": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_LOCATION / Path("factor_model_estimates.sqlite3"),
+        "TEST": {"DEPENDENCIES": [], "NAME": "test_factor_model_estimates"},
     },
-    'specific_risk_estimates':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_LOCATION / Path('specific_risk_estimates.sqlite3'),
-        'TEST': {'DEPENDENCIES': [],},
-    }
+    "specific_risk_estimates": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_LOCATION / Path("specific_risk_estimates.sqlite3"),
+        "TEST": {
+            "DEPENDENCIES": [],
+        },
+    },
 }
 
 
@@ -115,16 +118,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -132,9 +135,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -144,18 +147,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "frontend/dist"),
-    os.path.join(BASE_DIR, "frontend/src"),
+    os.path.join(BASE_DIR, "frontend"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-TEST_RUNNER = 'crypto_calculator.scripts.model_test_runner.ManagedModelTestRunner'
+TEST_RUNNER = "crypto_calculator.scripts.model_test_runner.ManagedModelTestRunner"
--- database_server/urls.py
+++ database_server/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.urls import re_path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("", TemplateView.as_view(template_name="index.html")),
-    path('crypto/', include('crypto_calculator.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html"))  # the ordering matters
-]
+    path("crypto/", include("crypto_calculator.urls")),
+    re_path(
+        r"^.*$", TemplateView.as_view(template_name="index.html")
+    ),  # the ordering matters
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
